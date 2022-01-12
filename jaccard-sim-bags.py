301426ebae80cc96ccd3a6e8f5bda6b419b3bc5e
Hussam Hallak <hhallak@cs.odu.edu>
2022-01-12 10:25:58 -0500

    Update jaccard-sim-bags.py

--- jaccard-sim-bags.py
+++ jaccard-sim-bags.py
@@ -1,18 +1,11 @@
 #Jaccard Similarity of Bags
+
+from collections import Counter
   
 #Returns the number of elements of x and y intersection
 def find_intersection_size(x: list, y: list) -> int:
-    size = 0
-    fewest, most = (x, y) if len(x) < len(y) else (y, x)
-    most = most.copy()
-    for value in fewest:
-        try:
-            most.remove(value)
-        except ValueError:
-            pass
-        else:
-            size += 1
-    return size
+    intersection = list((Counter(x) & Counter(y)).elements())
+    return len(intersection)
 
 #Return the number of elements in both x and y.
 def find_union_size(x: list, y: list) -> int:
@@ -25,13 +18,3 @@ def find_jaccard_similarity_of_bags(x: list, y: list) -> float:
     return (intersection_size/union_size)  
 
 if __name__ == "__main__":
-  
-      """Get the Jaccard similarity of two bags.
-    Example:
-        >>> jaccard_similarity_bags([1,1,1,2], [1,1,2,2,3])
-        0.3333333333333333
-        >>> jaccard_similarity_bags([1,1,1,2], [1,2,3,4])
-        0.25
-        >>> jaccard_similarity_bags([1,1,2,2,3], [1,2,3,4])
-        0.3333333333333333
-    """
